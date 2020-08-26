b28bc0ec5b8d2f8aa44bd383b8bd134d612cf8e6
WackyWouter <37797983+WackyWouter@users.noreply.github.com>
2020-08-26 21:09:39 +0100

    Update flutter_speedometer.dart

--- lib/flutter_speedometer.dart
+++ lib/flutter_speedometer.dart
@@ -76,109 +76,109 @@ class _SpeedometerState extends State<Speedometer> {
             width: _size,
             height: _size,
             child: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(_size * 0.075),
-                  child: Stack(children: <Widget>[
-                    Container(
-                      alignment: Alignment.center,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: _size,
-                        height: _size,
-                        // decoration: new BoxDecoration(
-                        //   color: widget.backgroundColor,
-                        //   boxShadow: [
-                        //     new BoxShadow(
-                        //         color: widget.kimColor,
-                        //         blurRadius: 8.0,
-                        //         spreadRadius: 4.0)
-                        //   ],
-                        //   shape: BoxShape.circle,
-                        // ),
-                      ),
-                    ),
-                    CustomPaint(
-                      size: Size(_size, _size),
-                      painter: ArcPainter(
-                          startAngle: 9,
-                          sweepAngle: 18,
-                          color: widget.warningColor),
-                    ),
-                    CustomPaint(
-                      size: Size(_size, _size),
-                      painter: ArcPainter(
-                          startAngle: 9,
-                          sweepAngle: _warningAngle,
-                          color: widget.meterColor),
-                    ),
-                  ]),
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: ClipPath(
-                    clipper: TriangleClipper(),
-                    child: Container(
-                      width: _size,
-                      height: _size * 0.5,
-                      color: widget.backgroundColor,
-                    ),
-                  ),
-                ),
+//               children: <Widget>[
 //                 Container(
 //                   alignment: Alignment.center,
-//                   child: Container(
-//                     width: _size * 0.1,
-//                     height: _size * 0.1,
-//                     alignment: Alignment.center,
-//                     decoration: new BoxDecoration(
-//                       color: widget.kimColor,
-//                       boxShadow: [
-//                         new BoxShadow(
-//                             color: widget.meterColor,
-//                             blurRadius: 10.0,
-//                             spreadRadius: 5.0)
-//                       ],
-//                       shape: BoxShape.circle,
-//                     ),
-//                   ),
-//                 ),
-//                 Container(
-//                   alignment: Alignment.center,
-//                   child: Transform.rotate(
-//                     angle: math.pi / 12 * _kimAngle,
-//                     child: ClipPath(
-//                       clipper: KimClipper(),
+//                   padding: EdgeInsets.all(_size * 0.075),
+//                   child: Stack(children: <Widget>[
+//                     Container(
+//                       alignment: Alignment.center,
 //                       child: Container(
-//                         width: _size * 0.9,
-//                         height: _size * 0.9,
-//                         color: widget.kimColor,
+//                         alignment: Alignment.center,
+//                         width: _size,
+//                         height: _size,
+//                         // decoration: new BoxDecoration(
+//                         //   color: widget.backgroundColor,
+//                         //   boxShadow: [
+//                         //     new BoxShadow(
+//                         //         color: widget.kimColor,
+//                         //         blurRadius: 8.0,
+//                         //         spreadRadius: 4.0)
+//                         //   ],
+//                         //   shape: BoxShape.circle,
+//                         // ),
 //                       ),
 //                     ),
+//                     CustomPaint(
+//                       size: Size(_size, _size),
+//                       painter: ArcPainter(
+//                           startAngle: 9,
+//                           sweepAngle: 18,
+//                           color: widget.warningColor),
+//                     ),
+//                     CustomPaint(
+//                       size: Size(_size, _size),
+//                       painter: ArcPainter(
+//                           startAngle: 9,
+//                           sweepAngle: _warningAngle,
+//                           color: widget.meterColor),
+//                     ),
+//                   ]),
+//                 ),
+//                 Container(
+//                   alignment: Alignment.bottomCenter,
+//                   child: ClipPath(
+//                     clipper: TriangleClipper(),
+//                     child: Container(
+//                       width: _size,
+//                       height: _size * 0.5,
+//                       color: widget.backgroundColor,
+//                     ),
 //                   ),
 //                 ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: Text(
-                      widget.displayText,
-                      style: widget.displayTextStyle,
-                    ),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, _size * 0.1),
-                    child: Text(
-                      widget.currentValue.toString(),
-                      style: widget.displayNumericStyle,
-                    ),
-                  ),
-                ),
-              ],
+// //                 Container(
+// //                   alignment: Alignment.center,
+// //                   child: Container(
+// //                     width: _size * 0.1,
+// //                     height: _size * 0.1,
+// //                     alignment: Alignment.center,
+// //                     decoration: new BoxDecoration(
+// //                       color: widget.kimColor,
+// //                       boxShadow: [
+// //                         new BoxShadow(
+// //                             color: widget.meterColor,
+// //                             blurRadius: 10.0,
+// //                             spreadRadius: 5.0)
+// //                       ],
+// //                       shape: BoxShape.circle,
+// //                     ),
+// //                   ),
+// //                 ),
+// //                 Container(
+// //                   alignment: Alignment.center,
+// //                   child: Transform.rotate(
+// //                     angle: math.pi / 12 * _kimAngle,
+// //                     child: ClipPath(
+// //                       clipper: KimClipper(),
+// //                       child: Container(
+// //                         width: _size * 0.9,
+// //                         height: _size * 0.9,
+// //                         color: widget.kimColor,
+// //                       ),
+// //                     ),
+// //                   ),
+// //                 ),
+//                 Container(
+//                   alignment: Alignment.bottomCenter,
+//                   child: Padding(
+//                     padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+//                     child: Text(
+//                       widget.displayText,
+//                       style: widget.displayTextStyle,
+//                     ),
+//                   ),
+//                 ),
+//                 Container(
+//                   alignment: Alignment.bottomCenter,
+//                   child: Padding(
+//                     padding: EdgeInsets.fromLTRB(0, 0, 0, _size * 0.1),
+//                     child: Text(
+//                       widget.currentValue.toString(),
+//                       style: widget.displayNumericStyle,
+//                     ),
+//                   ),
+//                 ),
+//               ],
             ),
           ),
         ),
